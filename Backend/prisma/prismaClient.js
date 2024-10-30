dc5b93aab505149e3a701cbb0652e1c2852ae549
Ishay Ezagouri <ishay7@gmail.com>
2024-10-30 17:59:15 +0200

    edited prismaclient.js

--- Backend/prisma/prismaClient.js
+++ Backend/prisma/prismaClient.js
@@ -1,4 +1,17 @@
 // prismaClient.js
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+async function connectPrisma() {
+  try {
+    await prisma.$connect();
+    console.log('Prisma Client connected successfully');
+  } catch (error) {
+    console.error('Error connecting Prisma Client:', error);
+    process.exit(1);
+  }
+}
+
+connectPrisma();
+
 module.exports = prisma;
